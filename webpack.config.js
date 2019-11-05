7a4ab4e2332cf1010ba3388f5a6deb231de7d0db
王叨叨 <hi@wangdaodao.com>
2019-11-05 14:32:08 +0800

    禁止生成source-map

--- webpack.config.js
+++ webpack.config.js
@@ -103,7 +103,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
